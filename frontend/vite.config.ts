8aae03db02db661e5266dbb58ae9addee2fd76e2
mooncake <mail@mooncaketv.com>
2025-08-19 20:49:10 -0700

    feat: install shadcn components

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,4 +5,9 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": new URL("./src", import.meta.url).pathname,
+    },
+  },
 });
