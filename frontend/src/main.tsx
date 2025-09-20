cf63525a4722057447975bcf4a992d33d3043bcc
mooncake <mail@mooncaketv.com>
2025-09-20 12:05:29 -0700

    refactor: remove unused App import from main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import "./style.css";
-import App from "./App";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 
 // Import the generated route tree
